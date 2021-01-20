676475e623e4de03eccfdf04c3e2f82596284f1e
James Wu <ywu@lcs.on.ca>
2021-01-20 10:11:30 -0500

    Initial Commit

---
+++ Shared/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Shared
+//
+//  Created by James Wu on 2021-01-20.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
