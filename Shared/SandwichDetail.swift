eb773cf558bb7fe88ef1ada3306d3d9f8fefc705
James Wu <ywu@lcs.on.ca>
2021-01-21 12:50:21 -0500

    Add state property to navigate the picture: tap to zoom  in

--- Shared/SandwichDetail.swift
+++ Shared/SandwichDetail.swift
@@ -9,19 +9,26 @@ import SwiftUI
 
 struct SandwichDetail: View {
     var sandwich: Sandwich
+    @State private var zoomed = false
     
     var body: some View {
         Image(sandwich.imageName)
             .resizable()
-            .aspectRatio(contentMode:.fit)
-            .navigationTitle(sandwich.name)
-      }
-}
-    
-    struct SandwichDetail_Previews: PreviewProvider {
-        static var previews: some View {
-            NavigationView{
-                SandwichDetail(sandwich: testData[0])
+            .aspectRatio(contentMode: zoomed ? .fill : .fit)
+            .onTapGesture {
+                withAnimation {
+                    zoomed.toggle()
+                }
             }
+            .navigationTitle(sandwich.name)
+            .edgesIgnoringSafeArea(.bottom)
+    }
+}
+
+struct SandwichDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            SandwichDetail(sandwich: testData[0])
+        }
     }
 }
