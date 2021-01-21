ddbc2b23a4edac3df830307c58941b41adaab158
James Wu <ywu@lcs.on.ca>
2021-01-21 16:54:09 -0500

    Add the banner showing whether the sandwich is spicy, and make it only appears in a detail view of a spicy sandwich and not zoomed

--- Shared/SandwichDetail.swift
+++ Shared/SandwichDetail.swift
@@ -12,23 +12,46 @@ struct SandwichDetail: View {
     @State private var zoomed = false
     
     var body: some View {
-        Image(sandwich.imageName)
-            .resizable()
-            .aspectRatio(contentMode: zoomed ? .fill : .fit)
-            .onTapGesture {
-                withAnimation {
-                    zoomed.toggle()
+        VStack {
+            Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+            Image(sandwich.imageName)
+                .resizable()
+                .aspectRatio(contentMode: zoomed ? .fill : .fit)
+                .onTapGesture {
+                    withAnimation {
+                        zoomed.toggle()
+                    }
                 }
+            Spacer(minLength: 0)
+            
+            if sandwich.isSpicy && !zoomed{
+                HStack {
+                    Spacer()
+                    Label("Spicy", systemImage:"flame.fill")
+                    Spacer()
+                }
+                .padding(.all)
+                .font(Font.headline.smallCaps())
+                .background(Color.red)
+                .foregroundColor(.yellow)
+                .transition(.move(edge: .bottom))
             }
-            .navigationTitle(sandwich.name)
-            .edgesIgnoringSafeArea(.bottom)
+            
+        }
+        .navigationTitle(sandwich.name)
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
 struct SandwichDetail_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView{
-            SandwichDetail(sandwich: testData[0])
+        Group {
+            NavigationView{
+                SandwichDetail(sandwich: testData[1])
+            }
+            NavigationView{
+                SandwichDetail(sandwich: testData[0])
+            }
         }
     }
 }
