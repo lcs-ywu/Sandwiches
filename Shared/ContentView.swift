d039f26ffff8c3aef953da8945b46b3bf4b8c7a4
James Wu <ywu@lcs.on.ca>
2021-01-21 16:59:13 -0500

    Add a text holder for iPad

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -23,6 +23,8 @@ struct ContentView: View {
                 foregroundColor(.secondary)
             }
             .navigationTitle("Sandwiches")
+            Text("Select a sandwich")
+                .font(.largeTitle)
         }
     }
 }
